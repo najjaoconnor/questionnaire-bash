67e9c73ba4e3920ed46d55bf52d59162f6378e0a
Najja O'Connor <najjaoconnor@Najjas-MacBook-Pro.local>
2023-03-12 16:39:51 -0400

    completed echo statement

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,3 +1,15 @@
-/usr/bin/bash
+#!/usr/bin/env bash
 
-echo "~~Questionnaire~~"
+echo -e "/n ~~Questionnaire~~ /n"
+
+QUESTION1= "What is your name?"
+read NAME
+
+QUESTION2= "Where do you live currenrly (City, State, Zip Code)"
+read LOCATION
+
+QUESTION3= "When were you born?"
+read BIRTHDATE
+
+echo Your Name is $NAME , you live at $LOCATION and you were born $BIRTHDATE.
+
